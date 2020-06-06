b74f356c56b2057253310b3c4640e8e5e3a4ea45
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-06 20:57:25 +0200

    task1+

--- 01_Podstawy_jezyka/04_Typy podstawowe/src/obiekty.js
+++ 01_Podstawy_jezyka/04_Typy podstawowe/src/obiekty.js
@@ -3,6 +3,8 @@ function Builder() {
   this.x = 'Hello';
 }
 const myObj = new Builder();
+const myObj2 = new Builder();
+
 Builder.prototype.y = 'World!';
 Builder.prototype.objProp = {};
 
@@ -14,3 +16,15 @@ console.log(myObj.x,'',myObj.y);
 
 // Funkcja tak naprawdę jest konstruktorem swoich "dzieci" - jej prototyp stanie się właściwościami obiektów z niej zbudowanych
 console.log(Builder === Builder.prototype.constructor); // true
+console.log(myObj.objProp === Builder.prototype.objProp); 
+Builder.prototype.y = "Tomek";
+console.log(myObj2.y);
+myObj2.x = "Nara";
+console.log(myObj2.x, '', myObj2.y);
+console.log(myObj2);
+
+
+
+
+
+
