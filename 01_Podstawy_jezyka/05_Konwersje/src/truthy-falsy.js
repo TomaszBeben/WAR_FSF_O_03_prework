6dec20faf0ce0dfad9013021faf41f59cd2586e8
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-06 21:31:52 +0200

    truthy/falsy

--- 01_Podstawy_jezyka/05_Konwersje/src/truthy-falsy.js
+++ 01_Podstawy_jezyka/05_Konwersje/src/truthy-falsy.js
@@ -1,8 +1,15 @@
 // Skopiuj poniższą linijkę Boolean() i podstaw za `true` jak najwięcej wartości innych typów
 console.log('--- TRUE: ---');
 console.log(Boolean(true));
+console.log(Boolean(2));
+console.log(Boolean("2"));
+console.log(Boolean(2.1));
+console.log(Boolean(Boolean));
 
 // Skopiuj poniższą linijkę Boolean() i podstaw za `false` jak najwięcej wartości innych typów
 console.log('--- FALSE: ---');
 console.log(Boolean(false));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(0));
 
