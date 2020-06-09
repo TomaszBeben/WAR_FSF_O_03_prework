e08c5dd954958929c82e178ade63318491ca9d32
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-09 22:16:17 +0200

    call,apply

--- 02_Funkcyjnosc/03_Call i apply/birds.js
+++ 02_Funkcyjnosc/03_Call i apply/birds.js
@@ -1,10 +1,29 @@
 // Możesz użyć tego obiektu jako bazy do kolejnych ptaków
-const birdSpeciesObj = {
-  species: '',
-  dominantColor: '',
-  size: '' // small || medium || large
+const birdOne = {
+  species: 'sikorak bogatka',
+  dominantColor: 'yellow',
+  size: 'medium', // small || medium || large
+  certainty: 0.55
 };
-
+const birdTwo = {
+  species: 'wróbel domowy',
+  dominantColor: 'grey',
+  size: 'small', // small || medium  || large
+  certainty: 0.2
+};
+const birdThree = {
+  species: 'dzięcioł duży',
+  dominantColor: 'grey',
+  size: 'medium', // small || medium  || large
+  certainty: 0.6
+};
+const birdFour = {
+  species: 'sokół',
+  dominantColor: 'brown',
+  size: 'large', // small || medium || large
+  certainty: 0.8
+};
+const date = new Date();
 function reportBird(date, certainty) {
   if (!(date instanceof Date)) {
     throw new Error('Passed date is not a proper Date object!');
@@ -25,3 +44,16 @@ function reportBird(date, certainty) {
     ', recognition certainty: ' + certainty
   )
 }
+console.log(reportBird.call(birdOne, new Date(), birdOne.certainty ))
+console.log(reportBird.apply(birdOne, [new Date(), birdOne.certainty] ))
+
+console.log(reportBird.call(birdTwo, new Date(), birdTwo.certainty ))
+console.log(reportBird.apply(birdTwo, [new Date(), birdTwo.certainty] ))
+
+console.log(reportBird.call(birdThree, new Date(), birdThree.certainty ))
+console.log(reportBird.apply(birdThree, [new Date(), birdThree.certainty] ))
+
+console.log(reportBird.call(birdFour, new Date(), birdFour.certainty ))
+console.log(reportBird.apply(birdFour, [new Date(), birdFour.certainty] ))
+
+
