ea9c8cfb5655c82bf0e514f06e253243bbf7076c
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-10 20:33:25 +0200

    ????

--- 03_Kontrola_przeplywu/03_Wartości domyślne/robot.js
+++ 03_Kontrola_przeplywu/03_Wartości domyślne/robot.js
@@ -13,13 +13,21 @@ function emitSound() {
 }
 
 function robotAction(input, shouldEmitSound) {
-  const action = actionsMap[input]; // Wybierz akcję
-  emitSound(); // Spróbuj wygenerować dźwięk
-
+  let action = '';
+  if(shouldEmitSound === true){
+  action = actionsMap[input] && emitSound();; // Wybierz akcję
+  }else{
+  action = actionsMap[input] && DEFAULT_ACTION;
+  }
   return action;
 }
+console.log(robotAction('shout', true));
+console.log(robotAction('push', true));
+console.log(robotAction('call', true));
 
-console.assert(robotAction('shout', false) === 'MOVE BACK', 'Wrong reaction!');
-console.assert(robotAction('push', true) === 'TWIST AROUND', 'Wrong reaction!');
-console.assert(robotAction('waveLeft', false) === 'MOVE LEFT', 'Wrong reaction!');
-console.assert(robotAction('call', true) === 'MOVE FORWARD', 'Wrong reaction!');
+
+
+// console.assert(robotAction('shout', true) === 'MOVE BACK', 'Wrong reaction!');
+// console.assert(robotAction('push', true) === 'TWIST AROUND', 'Wrong reaction!');
+// console.assert(robotAction('waveLeft', false) === 'MOVE LEFT', 'Wrong reaction!');
+// console.assert(robotAction('call', true) === 'MOVE FORWARD', 'Wrong reaction!');
