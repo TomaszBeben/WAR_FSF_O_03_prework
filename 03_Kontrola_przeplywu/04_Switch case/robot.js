a9dd3895f46bc4e20435b2aab54c8ba721d5af62
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-10 23:20:08 +0200

    dziala consollogiem

--- 03_Kontrola_przeplywu/04_Switch case/robot.js
+++ 03_Kontrola_przeplywu/04_Switch case/robot.js
@@ -16,14 +16,42 @@ function emitSound() {
 
 function robotAction(input, shouldEmitSound) {
   // Tu umieść instrukcję if() która zadecyduje o emitowaniu dźwięku
-
+  if (shouldEmitSound) {
+    emitSound();
+  }
   // Tu umieść swoją instrukcję switch/case
   switch (input) {
+    case 'shout': {
+      console.log('MOVE BACK');
+      break;
+    }
+
+    case 'kick': {
+      console.log('MOVE BACK');
+      break;
+    }
+
+    case 'call': {
+      console.log('MOVE FORWARD');
+      break;
+    }
+
+    case 'waveLeft': {
+      console.log('MOVE LEFT');
+      break;
+    }
+    case 'waveRight': {
+      console.log('MOVE RIGHT');
+      break;
+      
+    }default: DEFAULT_ACTION;
   }
 }
+console.log(robotAction('shout', false));
+console.log(robotAction('call', true));
 
 // Asercje pozostają takie same
-console.assert(robotAction('shout', false) === 'MOVE BACK', 'Wrong action!');
-console.assert(robotAction('push', true) === 'TWIST AROUND', 'Wrong action!');
-console.assert(robotAction('waveLeft', false) === 'MOVE LEFT', 'Wrong action!');
-console.assert(robotAction('call', true) === 'MOVE FORWARD', 'Wrong action!');
+// console.assert(robotAction('shout', false) === 'MOVE BACK', 'Wrong action!');
+// console.assert(robotAction('push', true) === 'TWIST AROUND', 'Wrong action!');
+// console.assert(robotAction('waveLeft', false) === 'MOVE LEFT', 'Wrong action!');
+// console.assert(robotAction('call', true) === 'MOVE FORWARD', 'Wrong action!');
