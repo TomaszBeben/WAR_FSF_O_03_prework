8d270a99821ec52b41cd204130a86c697affed3d
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-12 17:18:06 +0200

    for

--- 03_Kontrola_przeplywu/05_For/checkerboard.js
+++ 03_Kontrola_przeplywu/05_For/checkerboard.js
@@ -6,10 +6,31 @@ console.log(15 % 2);
 console.log(10 % 2);
 
 function generateCheckerboard(times, blackTileChar, whiteTileChar) {
+    let sign = "";
+    for (let i = 0; i < times; i++) {
+        sign = sign + '\n'
+        for (let j = 0; j < times; j++) {
+            if (i % 2 === 1) {
+                if (j % 2 === 1) {
+                    sign = sign + whiteTileChar;
+                } else if (j % 2 === 0) {
+                    sign = sign + blackTileChar;
+                }
+            }else {
+                if(j % 2 === 0){
+                    sign = sign + blackTileChar
+                }else if (j % 2 === 1){
+                    sign = sign + whiteTileChar
+                }
+            }
+        }
+    }
+    console.log(sign);
+
 }
 
 console.log('\n/**************/');
 console.log('| CHECKERBOARD |');
 console.log('/**************/');
 
-generateCheckerboard(8, '##', '__');
+generateCheckerboard(10, '##', '__');
