f23f3e1e2bd4c34d700b50c77ecba8e2a1d42c46
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-15 17:46:35 +0200

    operatory

--- 03_Kontrola_przeplywu/10_Operatory typów/club.js
+++ 03_Kontrola_przeplywu/10_Operatory typów/club.js
@@ -20,7 +20,23 @@ class PartyGoer extends Person {
 
 function selector(clubGoers) {
   // Tutaj zaimplementuj swoją logikę
+  let allowed = true;
+  if (Array.isArray(clubGoers)) {
+
+    clubGoers.forEach(person => {
+      if (person.greeting === 'I should not be in the club...') {
+        allowed = false;
+      }
+    })
+    return allowed
+
+  }
+  else if (clubGoers.greeting === 'I should not be in the club...') {
+    allowed = false;
+  }
+  return allowed;
 }
+
 
 const firstGroup = [
   new PersonaNonGrata(),
@@ -28,16 +44,23 @@ const firstGroup = [
   new PartyGoer(),
 ];
 console.assert(selector(firstGroup) === false, 'Should not enter the club!');
+console.log(selector(firstGroup));
+
+
 
 const secondGroup = [
   new PartyGoer(),
   new PartyGoer(),
   new PartyGoer(),
 ];
+
 console.assert(selector(secondGroup) === true, 'Should enter the club!');
-
+console.log(selector(secondGroup));
 const thirdGroup = new PersonaNonGrata();
-console.assert(selector(thirdGroup) === false, 'Should not enter the club!');
 
+console.assert(selector(thirdGroup) === false, 'Should not enter the club!');
+console.log(selector(thirdGroup));
 const fourthGroup = new PartyGoer();
+
 console.assert(selector(fourthGroup) === true, 'Should enter the club!');
+console.log(selector(fourthGroup));
