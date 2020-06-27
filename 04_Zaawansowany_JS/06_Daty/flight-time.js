e9886b6d1d82417b5cbce7dd1afb80da3f48575c
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-27 15:14:08 +0200

    DATE

--- 04_Zaawansowany_JS/06_Daty/flight-time.js
+++ 04_Zaawansowany_JS/06_Daty/flight-time.js
@@ -4,9 +4,21 @@ const departureTime = `2020-10-10T12:30+01:00`;
  * Modify this function to return proper arrival for Larry
  */
 function calculateArrival(departure) {
-  return departure;
+  let arrival = new Date (departure);
+  console.log(arrival);
+  
+  arrival.setHours(arrival.getHours() + 20);
+  console.log(arrival);
+  
+  arrival.setMinutes(arrival.getMinutes() + 40);
+  console.log(arrival.toLocaleString('pl-PL', { timeZone: 'Europe/Warsaw'}));
+  console.log(arrival.toLocaleString('pl-PL', { timeZone: 'Australia/Sydney'}));
+  
+  
+  return arrival.toLocaleString('pl-PL', { timeZone: 'Australia/Sydney'});
 }
 
 const arrivalTime = calculateArrival(departureTime);
 
 console.assert(arrivalTime === '11.10.2020, 19:10:00', 'Wrong arrival time: ', arrivalTime);
+
