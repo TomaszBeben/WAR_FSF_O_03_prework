ca20cfdc164b34ee9cae3be1f7de8ac904a950ec
TomaszBeben <tbeben@st.swps.edu.pl>
2020-06-28 20:11:18 +0200

    sword

--- 05_ES6_-_nowe_API/01_Array API/sword.js
+++ 05_ES6_-_nowe_API/01_Array API/sword.js
@@ -6,13 +6,23 @@ const SWORD_PATTERN = '{}oo((X))>';
  */
 function sword(length) {
   // First = create array from sword pattern
-
+  const sword = Array.from(SWORD_PATTERN);
+  console.log(sword);
+  
   // Second - create two arrays from sword pattern array
-
+  const handle = sword.slice(0,9)
+  console.log(handle);
+  const tip = sword.slice(9, sword.length)
+  console.log(tip);
+  
+  
   // Third = create blade pattern, using `Array(x).fill(''); with proper length and character
+  const blade = Array(length).fill(BLADE_CHARACTER);
+
 
   // Fourth - concatenate the arrays and join with empty character, using `join('')`
-  return SWORD_PATTERN;
+  const solve = handle.concat(blade, tip).join('')
+  return solve
 }
 
 const shortSword = sword(5);
